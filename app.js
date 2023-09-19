69e723f08b366fa0e56adf53e4c7f8d466e7295b
Four <105184275+MikeMWD05@users.noreply.github.com>
2023-09-19 11:42:50 +0200

    Oefening 4 af

---
+++ app.js
@@ -0,0 +1,11 @@
+const utils = require('./utils.js');
+
+const result = utils.sum(4,4);
+const result2 = utils.divide(8,2);
+const result3 = utils.substract(5,3);
+const result4 = utils.multiply(4,3);
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
---
+++ utils.js
@@ -0,0 +1,22 @@
+const sum = function(a,b) {
+    return (a+b)
+}
+
+const multiply = function(a,b) {
+    return (a*b)
+}
+
+const divide = function(a,b) {
+    return (a/b)
+}
+
+const substract = function(a,b) {
+    return (a-b)
+}
+
+module.exports = {
+    sum,
+    multiply,
+    divide,
+    substract
+}
